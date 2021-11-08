d91b42b2a3260c111543e86b7325f1bf3a758347
evfjunior <evfjunior@protonmail.com>
2021-11-08 02:03:03 -0300

    feat: create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  ${() => css`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+    }
+
+    *::after,
+    *::before {
+      box-sizing: inherit;
+    }
+
+    body,
+    html,
+    #__next {
+      height: 100%;
+    }
+
+    html {
+      min-width: 320px;
+      font-size: 62.5%;
+    }
+
+    body {
+      background-color: #212121;
+      color: #fafafa;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      font-size: 1rem;
+      scroll-behavior: smooth;
+    }
+
+    a,
+    button,
+    input,
+    textarea,
+    select {
+      font: inherit;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    button {
+      -webkit-tap-highlight-color: transparent;
+    }
+  `}
+`
