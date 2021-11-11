759dc9c08edb37b69b7612ca1e66ba7d3e464b17
evfjunior <evfjunior@protonmail.com>
2021-11-11 07:45:03 -0300

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: { builder: 'webpack5' },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
+}
