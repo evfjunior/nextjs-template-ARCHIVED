ae9177e76eb6e22ea49e133ed6a9a20885e7d678
evfjunior <evfjunior@protonmail.com>
2021-11-22 10:46:21 -0300

    feat: add google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
