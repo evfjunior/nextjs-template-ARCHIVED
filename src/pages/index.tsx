110b103ac6e1414df72988bfc629ad167e2f00ac
evfjunior <evfjunior@protonmail.com>
2021-11-22 11:00:02 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { Card } from 'components/Card'
+import { Main } from 'components/Main'
+
 export default function Home() {
   return (
-    <div>
-      <h1>NextJS Template</h1>
-    </div>
+    <Main>
+      <Card
+        hero
+        heading="NextJS Template"
+        leading="A complete project structure to create React web applications using NextJS and Typescript "
+      />
+    </Main>
   )
 }
