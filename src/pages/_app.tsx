205ef4e18c487e6b35648f4ac744fcb15ee552c6
evfjunior <evfjunior@protonmail.com>
2021-11-22 10:19:56 -0300

    chore: apply theme to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyles } from 'styles/global'
+import { theme } from 'styles'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   )
 }
 
-export default MyApp
+export default App
